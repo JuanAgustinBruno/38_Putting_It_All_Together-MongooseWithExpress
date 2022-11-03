d234553ed57a50b1308455b682d39e9ca247a656
Agustin <equite.augusto@gmail.com>
2022-11-03 20:57:06 +0300

    changed from app.get /dog to /products + async await Products.find + new res

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get("/dog", (req,res) => {
-    res.send("woof!")
+app.get("/products", async (req,res) => {   
+    const products = await Product.find({})
+    console.log(products)
+    res.send("all products will be here")  
+
 })
 
 app.listen(3000, () => {
