2e0583e77b342b973eea602ae84ebcac2690fbb2
Agustin <equite.augusto@gmail.com>
2022-11-03 11:58:08 +0300

    added mongo connection to mongodb://localhost:27017/farmStand

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 const path = require('path');
 const mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
 app.set('views', path.join(__dirname, 'views'));
-
-
 app.set('view engine', 'ejs');
 
 app.get("/dog", (req,res) => {
