54a29a05d8686241e9bc62e2c693630a2200b2c2
Agustin <equite.augusto@gmail.com>
2022-11-03 21:34:59 +0300

    send product/:id

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get("/products", async (req,res) => {
 
 })
 
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    console.log(product);
+    res.send("details page")
+})
+
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
 })
