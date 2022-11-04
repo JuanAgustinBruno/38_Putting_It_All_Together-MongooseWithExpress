9d4df5a405f1532c6e99bda4ad3451c2c4c7f1c9
Agustin <equite.augusto@gmail.com>
2022-11-04 15:15:21 +0300

    added express.urlencoded(extended:true) and post /products , conslog , send confirmation to browser

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/products/new', (req, res) => {
-    res.render('products/new')
-})
+
+
 
 app.get("/products", async (req,res) => {   
     const products = await Product.find({})
@@ -27,6 +27,19 @@ app.get("/products", async (req,res) => {
     res.render("products/index", { products })
 
 })
+
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+
+app.post('/products', (req, res) => {
+    console.log(req.body)
+    res.send("making your product")
+})
+
+
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
